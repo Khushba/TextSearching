abf5cd1b0934bce0babe2bef9af359f74b5c0e8e
Khushba <51269013+Khushba@users.noreply.github.com>
2019-06-01 21:17:23 +0500

    Initial commit

---
+++ app/src/main/java/com/example/textsearching/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.textsearching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import java.util.ArrayList; // import the ArrayList class
+
+ArrayList<String> ayah=new ArrayList<String>(); // Create an ArrayList object
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+  cars.add("Volv");
+    System.out.println(cars);
+
+
+}
