119eb5a0f563d424d08e91f601cb60c343cda7f3
Khushba <51269013+Khushba@users.noreply.github.com>
2019-06-01 22:02:50 +0500

    okkkkkkk

--- app/src/main/java/com/example/textsearching/MainActivity.java
+++ app/src/main/java/com/example/textsearching/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-  cars.add("Volvoooooooooo");
+  cars.add("Volvoooooookjijkjkooo");
     System.out.println(cars);
 
 
